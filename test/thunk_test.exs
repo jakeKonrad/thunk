fda070c8965c3ab4cba2673b2b87fe7de6765f10
Jake Konrad <jakegkonrad@gmail.com>
2018-03-04 10:04:51 +0000

    Updated tests

--- test/thunk_test.exs
+++ test/thunk_test.exs
@@ -1,31 +1,17 @@
 defmodule ThunkTest do
-  use ExUnit.Case
-
+  use ExUnit.Case, async: true
   require Thunk
+  doctest Thunk, except: [:moduledoc]
 
-  test "Suspend does not evaluate argument" do
-    Thunk.suspend(raise("Oops, suspend evaluated its argument"))
+  test "Error propagation" do
+    thunk = Thunk.suspend(raise("Fail"))
+    thunk_copy = Thunk.copy(thunk)
+    assert_raise RuntimeError, fn -> Thunk.force(thunk_copy) end
   end
 
-  test "Force extracts value of thunk" do
-    thunk = Thunk.suspend(1)
-    assert Thunk.force(thunk) == 1
-  end
-
-  test "Map applies function to thunk" do
-    thunk1 = Thunk.suspend(1)
-    thunk2 = Thunk.map(thunk1, fn x -> x + 1 end)
-    assert Thunk.force(thunk2) == 2
-  end
-
-  test "Map does not force thunk" do
-    thunk = Thunk.suspend(raise("Oops, map evaluated the thunk"))
-    Thunk.map(thunk, fn _err -> nil end)
-  end
-
-  test "Map does not alter value of old thunk" do
-    thunk1 = Thunk.suspend(1)
-    _thunk2 = Thunk.map(thunk1, fn x -> x + 1 end)
-    assert Thunk.force(thunk1) == 1
+  test "Thunk deletion error" do
+    thunk = Thunk.suspend(:computation)
+    Thunk.delete(thunk)
+    assert_raise ThunkError, fn -> Thunk.force(thunk) end
   end
 end
